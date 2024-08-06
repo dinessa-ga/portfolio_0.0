89a208ce9b1cba9a02f89ea95ba8fbfded60fb6e
dinessa-ga <dinessa.ga@gmail.com>
2024-08-06 15:05:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <p className="text-lg">ESto</p>
+    
+        
+    </>
+  )
+}
+
+export default App
