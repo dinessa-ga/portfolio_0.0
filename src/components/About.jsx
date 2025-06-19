2382d6ec544825b1421e596168f91a1dd29905f0
Diana <dinessa.ga@gmail.com>
2025-06-19 15:41:26 -0500

    feat: change about navigation in footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,3 @@
-// function About() {
-//   return (
-//     <section id="about" className="my-8">
-//       <h2 id="about-title" className="text-3xl font-bold">Sobre mí</h2>
-//       <div
-//         id="about-info"
-//         className="flex flex-col md:flex-row items-center md:items-start gap-8 mt-6"
-//       >
-//         <div className="flex-1">
-//           <h3 className="text-lg mt-4">Creando soluciones centradas en la experiencia de usuario</h3>
-//           <p className="text-lg mt-4 ">
-//             Creo soluciones desde el análisis de procesos, tareas,
-//             diseños y la resolución de problemas.
-//             Mis años de experiencia en creación de contenido, conocimiento en diseño y desarrollo
-//             de aplicaciones web y móviles, me permiten crear soluciones centradas en la experiencia de
-//             usuario para diferentes tipos de negocios.
-//           </p>
-//         </div>
-//         <div className="flex-1 flex justify-center md:justify-end">
-//           <img
-//             src="about_me/yo.png"
-//             className="mt-4 w-40 md:w-64 rounded-lg object-cover"
-//             alt="Sobre mí"
-//           />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default About;
-
-
 function About() {
   return (
     <section
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,3 @@
-    import React, { useState } from 'react';
-
 const Contact = () => {
     const [formData, setFormData] = useState({
         name: '',
